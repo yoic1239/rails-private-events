04fa1fa4e660521b5f0be4a87fad07cfddc3e343
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-03-27 15:03:25 -0600

    feat: Implement Event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to event_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
+  end
 end
