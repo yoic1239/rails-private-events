357dcdb0e03958069ae6704eb6294f6b5a68e422
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-03-28 16:41:04 -0600

    feat: Add association between attendee and event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :event_attendances, foreign_key: "attended_event_id"
+  has_many :attendees, through: :event_attendances
 
   validates :name, presence: true
   validates :location, presence: true
