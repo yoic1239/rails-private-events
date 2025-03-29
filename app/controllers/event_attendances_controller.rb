72bd8bb761cd801b3a0a1cd7072d9ded3332a17d
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-03-29 13:01:07 -0600

    feat: Allow user to add attendance for the event

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,4 +1,12 @@
 class EventAttendancesController < ApplicationController
   def create
+    @event = Event.find(params[:event_id])
+    @attendance = @event.event_attendances.build(attendee: current_user)
+
+    if @attendance.save
+      redirect_to @event, notice: "You have successfully registered for this event."
+    else
+      redirect_to @event, alert: "Unable to register for the event."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [ :show ]
-  resources :events, only: [ :index, :new, :create, :show ]
-  resources :event_attendances, only: [ :create ]
+  resources :events, only: [ :index, :new, :create, :show ] do
+    post "/attendance", to: "event_attendances#create"
+  end
 
   root "events#index"
 end
