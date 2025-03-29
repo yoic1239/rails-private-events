0f920ef3840be665032af566d43b45c529a2886e
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2025-03-28 18:47:40 -0600

    feat: Destroy dependent when user or event deleted

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :event_attendances, foreign_key: "attended_event_id"
+  has_many :event_attendances, foreign_key: "attended_event_id", dependent: :destroy
   has_many :attendees, through: :event_attendances
 
   validates :name, presence: true
